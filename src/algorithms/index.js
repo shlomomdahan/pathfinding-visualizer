c97cbbe22b530b922548949b3db1eb2663439528
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-03-17 10:42:59 -0400

    fix astar

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -1,24 +1,9 @@
 import { getNodesInShortestPathOrder } from "./algoHelper";
 
-// export const executeAlgorithm = (algorithm, grid, startNode, finishNode) => {
-//   const visitedNodesInOrder = algorithm(grid, startNode, finishNode);
-//   // get the finish node from the grid visited by the algorithm
-//   const finish = visitedNodesInOrder.find((node) => node.isFinish);
-//   const nodesInShortestPathOrder = getNodesInShortestPathOrder(finish);
-//   return { visitedNodesInOrder, nodesInShortestPathOrder };
-// };
-
 export const executeAlgorithm = (algorithm, grid, startNode, finishNode) => {
-  const visitedNodesInOrder = algorithm(grid, startNode, finishNode) || [];
-  console.log(visitedNodesInOrder); // Check what you're getting here
-
-  const finish = visitedNodesInOrder.find(
-    (node) => node.isFinish && node.isVisited
-  );
-  if (!finish) {
-    return { visitedNodesInOrder, nodesInShortestPathOrder: [] };
-  }
-
+  const visitedNodesInOrder = algorithm(grid, startNode, finishNode);
+  // get the finish node from the grid visited by the algorithm
+  const finish = visitedNodesInOrder.find((node) => node.isFinish);
   const nodesInShortestPathOrder = getNodesInShortestPathOrder(finish);
   return { visitedNodesInOrder, nodesInShortestPathOrder };
 };
